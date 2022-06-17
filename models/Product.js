ddb8530745f5dce2e6b790e15abbf6cf1540d405
Jordan Thomas <jthomas135@hotmail.com>
2022-06-17 11:44:58 -0700

    added and successfully tested get route to findAll categories

--- models/Product.js
+++ models/Product.js
@@ -33,7 +33,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 5,
+      // defaultValue: 5,
       validate: {
         isNumeric: true
       }
